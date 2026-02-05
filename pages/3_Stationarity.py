9a3f5937e8c99273e07b24097b65d1866c1e834b
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-02-05 17:27:28 +0530

    Update 3_Stationarity.py

--- pages/3_Stationarity.py
+++ pages/3_Stationarity.py
@@ -1,1 +1,22 @@
 
+import streamlit as st
+from statsmodels.tsa.stattools import adfuller
+from utils.validator import load_data
+
+df = load_data()
+
+def adf(series):
+    result = adfuller(series.dropna())
+    return result[0], result[1]
+
+stat, pval = adf(df['Returns'])
+
+st.header("📐 Stationarity Test – ADF")
+
+st.metric("ADF Statistic", round(stat,4))
+st.metric("P-value", round(pval,4))
+
+if pval < 0.05:
+    st.success("✅ Returns are stationary")
+else:
+    st.error("❌ Non-stationary series")
