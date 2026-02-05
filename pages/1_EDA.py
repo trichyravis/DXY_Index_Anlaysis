4585eb439102a5104f0e079c566a309bb502ca9a
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-02-05 17:26:54 +0530

    Update 1_EDA.py

--- pages/1_EDA.py
+++ pages/1_EDA.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from utils.validator import load_data
+
+df = load_data()
+
+st.header("📊 Exploratory Data Analysis – DXY")
+
+st.subheader("DXY Level")
+st.line_chart(df['DXY'])
+
+st.subheader("Returns")
+st.line_chart(df['Returns'])
+
+st.subheader("Moving Averages")
+st.line_chart(df[['DXY','MA_50','MA_200']])
 
